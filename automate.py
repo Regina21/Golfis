089fe634eb1dd95f0b76735a318859229cde0566
Regina21 <47861350+Regina21@users.noreply.github.com>
2019-04-27 10:52:28 +0200

    Update automate.py

--- automate.py
+++ automate.py
@@ -1,50 +1,42 @@
 import twint
+from multiprocessing import Process
 import schedule
 import time
 
+# this runs a loop for each user in the users list
 
-# you can change the name of each "job" after "def" if you'd like.
-
-def jobone():
+def jobone(user):
 	print ("Fetching Tweets")
 	c = twint.Config()
-		# choose username (optional)
-	c.Username = "Dutzee"
-		# choose beginning time (narrow results)
-	c.Since = "2018-01-01"
-		# choose beginning time (narrow results)
-	c.Until = "2018-12-01"
-		# set limit on total tweets
+
+		# username is the list of users
+	c.Username = user
+
+		# beginning time (narrow results)
+	c.Since = "2017-01-01"
+
+		# ending time (narrow results)
+	c.Until = "2018-12-31"
+
+		# limit on total tweets
 	c.Limit = 1000
-		# no idea, but makes the csv format properly
+
+		# format of storage
 	c.Store_csv = True
-		# format of the csv
-		# c.Custom = ["date", "time", "username", "tweet", "link", "likes", "retweets", "replies", "mentions", "hashtags"]
-		# change the name of the csv file
-	c.Output = "Dutzee.csv"
+
+		# name of the csv file
+	c.Output = "twint"
+
 	twint.run.Search(c)
 
-#def jobtwo():
-#	print ("Fetching Tweets")
-#	c = twint.Config()
-		# choose username (optional)
-#	c.Username = "djokernole"
-		# choose search term (optional)
+# best 10 men & best 10 women of tennis
+users = ["djokernole", "RafaelNadal" "ThiemDomi", "rogerfederer", "keinishikori", "KAndersonATP",
+		"delpotrojuan", "JohnIsner", "StefsTsitsipas", "cilic_marin",
+		"Naomi_Osaka_", "Simona_Halep", "Petra_Kvitova", "KaPliskova",
+		"AngeliqueKerber", "ElinaSvitolina", "kikibertens", "SloaneStepehns",
+		"ashbar96", "SabalenkaA", "serenawilliams"]
 
-#	c.Since = "2018-01-01"
-		# set limit on total tweets
-#	c.Until = "2018-12-01"
-		# set limit on total tweets
-#	c.Limit = 1000
-		# no idea, but makes the csv format properly
-#	c.Store_csv = True
-		# format of the csv
-		#c.Custom = ["date", "time", "username", "tweet", "link", "likes", "retweets", "replies", "mentions", "hashtags"]
-		# change the name of the csv file
-#	c.Output = "djokernole2.csv"
-#	twint.run.Search(c)#
+for user in users:
 
-	# run once when you start the program
+	jobone(user)
 
-jobone()
-#jobtwo()
